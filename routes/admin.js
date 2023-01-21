7d6dbd50f651ca5d8cbed12e54e016e3a55af8f8
tarunAllmysons <tarun@in.allmysons.com>
2023-01-21 23:39:20 +0530

    updates

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,27 @@
-const express = require("express");
 const path = require("path");
+
+const express = require("express");
+
 const rootDir = require("../util/path");
 
 const router = express.Router();
 
-router.get("/add-product", (req, res, next) => {
-  // res.send(
-  //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Send</button></form>'
-  // );
+const products = [];
 
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
+  //res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
+// /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  //extracting info from the body
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
